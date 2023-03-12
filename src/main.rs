d56f464e70f066e10921b43e3ffcfcc05b4b2359
Karim-W <karim.wael@gmail.com>
2023-03-12 15:39:27 +0400

    feat. cmd line args for dynamic folder and file purge

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,65 @@
+use std::collections::HashMap;
 use std::path::Path;
-fn main() {
-    let current_path = Path::new(".");
-    recurse_clean(&current_path);
+
+struct FolderManager {
+    folders: HashMap<String, bool>,
+    files: HashMap<String, bool>,
 }
 
-fn recurse_clean(path: &Path) {
-    let folders_res = path.read_dir();
-    if folders_res.is_err() {
-        return;
+impl FolderManager {
+    pub fn new(folders: HashMap<String, bool>, files: HashMap<String, bool>) -> FolderManager {
+        FolderManager { folders, files }
     }
-    let folders = folders_res.unwrap();
-    for folder in folders {
-        if folder.is_err() {
-            continue;
+    pub fn recurse_clean(&self, path: &Path) {
+        let folders_res = path.read_dir();
+        if folders_res.is_err() {
+            return;
         }
-        let folder = folder.unwrap();
-        let path = folder.path();
-        if path.is_dir() {
-            if path.file_name().unwrap() == "target" {
-                println!("removing: {:?}", path);
-                std::fs::remove_dir_all(&path).unwrap();
+        let folders = folders_res.unwrap();
+        for folder in folders {
+            if folder.is_err() {
                 continue;
             }
-            if path.file_name().unwrap() == ".git" {
-                println!("removing: {:?}", path);
-                std::fs::remove_dir_all(&path).unwrap();
-                continue;
+            let folder = folder.unwrap();
+            let path = folder.path();
+            if path.is_dir() {
+                let is_folder_prob = self
+                    .folders
+                    .get(path.file_name().unwrap().to_str().unwrap());
+                if is_folder_prob.is_some() {
+                    let is_folder = is_folder_prob.unwrap();
+                    if *is_folder {
+                        println!("removing: {:?}", path);
+                        std::fs::remove_dir_all(&path).unwrap();
+                        continue;
+                    }
+                }
+                self.recurse_clean(&path);
+            } else {
+                let is_file_prob = self.files.get(path.file_name().unwrap().to_str().unwrap());
+                if is_file_prob.is_some() {
+                    println!("removing: {:?}", path);
+                    std::fs::remove_file(&path).unwrap();
+                }
             }
-            if path.file_name().unwrap() == "node_modules" {
-                println!("removing: {:?}", path);
-                std::fs::remove_dir_all(&path).unwrap();
-                continue;
-            }
-            recurse_clean(&path);
         }
     }
+}
+
+fn main() {
+    let current_path = Path::new(".");
+    let args: Vec<String> = std::env::args().collect();
+    let mut folders: HashMap<String, bool> = HashMap::new();
+    let mut files: HashMap<String, bool> = HashMap::new();
+    // if -d is passed, then the next argument is a folder to delete
+    // if -f is passed, then the next argument is a file to delete
+    for i in 0..args.len() {
+        if args[i] == "-d" {
+            folders.insert(args[i + 1].clone(), true);
+        }
+        if args[i] == "-f" {
+            files.insert(args[i + 1].clone(), true);
+        }
+    }
+    FolderManager::new(folders, files).recurse_clean(&current_path);
 }
